46d99e189845b43a0b468f5cb6f766f6ffaf9925
KevinYaguar <68433099+KevinYaguar@users.noreply.github.com>
2020-09-28 11:53:04 -0300

    estilos en footer

--- gifos.js
+++ gifos.js
@@ -1,6 +1,22 @@
+//Menu Hamburguesa a X
 let hamburguesa = document.getElementById('fa-bars');
 hamburguesa.addEventListener('click',() => {
     hamburguesa.classList.toggle('fa-times');
     hamburguesa.classList.toggle('fa-bars');
 })
+
+//Fetch imagenes trending
+
+
+function trending (){
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=1yVSM9oVX3z5nlo213gmodWDvoRxttsM&q=cats&limit=25&offset=0&rating=g&lang=en')
+        .then(res=> res.json())
+            .then(json => json.data)
+                .then(gif => gif[0])
+                    .then(final => {
+                        let primerImagen = document.getElementById('primer-imagen');
+                        primerImagen.setAttribute('src', final.url)
+                    })
+}
+trending();
 
