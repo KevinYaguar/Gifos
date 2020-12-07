600d61aea7785f34fc0f29c5dcce546afd61dba9
KevinYaguar <68433099+KevinYaguar@users.noreply.github.com>
2020-12-07 18:39:44 -0300

    correccion variable server

--- scripts/variables.js
+++ scripts/variables.js
@@ -4,9 +4,9 @@ const ApiKey = 'api_key=1yVSM9oVX3z5nlo213gmodWDvoRxttsM';
 let gifsGuardadosSinRepeticion = [];
 
 //Server
-let server = 'http://127.0.0.1:5502/';
+let server = 'https://kevinyaguar.github.io/';
 //http://127.0.0.1:5502/'
-let serverGitHub = 'http://127.0.0.1:5502/';
+let serverGitHub = 'https://kevinyaguar.github.io/';
 
 // Body y local storage
 let cuerpoWeb = document.getElementById('body'); //Body Completo.
